6a6bc9c5d27ec9af50c91c6fada7a4d5881f1a8d
tom <to_more@mailoo.org>
2019-06-03 20:56:35 -0300

    agrego build y settings de gradle

---
+++ build.gradle.kts
@@ -0,0 +1,56 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+val kotlinVersion = "1.3.21"
+
+plugins {
+	id("org.springframework.boot") version "2.1.5.RELEASE"
+	id("io.spring.dependency-management") version "1.0.7.RELEASE"
+	kotlin("jvm") version "1.3.21"
+	kotlin("plugin.spring") version "1.3.21"
+}
+
+group = "com.tiendanube"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_1_8
+
+repositories {
+	mavenCentral()
+}
+
+extra["springCloudVersion"] = "Greenwich.SR1"
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.springframework.cloud:spring-cloud-starter-sleuth")
+
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+	implementation("io.arrow-kt:arrow-core-data:0.9.0")
+	implementation("io.arrow-kt:arrow-typeclasses:0.9.0")
+	implementation("io.arrow-kt:arrow-effects-extensions:0.9.0")
+
+	runtimeOnly("org.postgresql:postgresql")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("io.kotlintest:kotlintest-runner-junit5:3.3.2")
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+	}
+}
+
+val test by tasks.getting(Test::class) {
+	useJUnitPlatform { }
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs = listOf("-Xjsr305=strict")
+		jvmTarget = "1.8"
+	}
+}
