9d55c6f2c7a3d8b7ac1f264550d36f46e59335a3
tom <to_more@mailoo.org>
2019-06-03 21:24:18 -0300

    agrego dao de merchant

---
+++ src/main/kotlin/com/tiendanube/challenge/daos/MerchantDao.kt
@@ -0,0 +1,92 @@
+package com.tiendanube.challenge.daos
+
+import com.tiendanube.challenge.model.Merchant
+import org.springframework.data.jdbc.repository.query.Modifying
+import org.springframework.data.jdbc.repository.query.Query
+import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.query.Param
+import java.time.LocalDate
+
+/**
+ * Created by tomReq on 6/3/19.
+ */
+interface MerchantDao: CrudRepository<Merchant, Long> {
+
+  @Modifying
+  @Query("""
+    INSERT /* class: MerchantDao, query: insert-merchant */ INTO merchants (id, name, creation_date, email, phone, address, balance, credit, plan_id)
+    VALUES (:id, :name, :creation_date, :email, :phone, :address, :balance, :credit, :plan_id)
+    """
+  )
+  fun create(
+    @Param("id") id: Long,
+    @Param("name") name: String,
+    @Param("creation_date") creationDate: LocalDate,
+    @Param("email") email: String,
+    @Param("phone") phone: String,
+    @Param("address") address: String,
+    @Param("balance") balance: Double,
+    @Param("credit") credit: Double,
+    @Param("plan_id") plan: Long?
+  ): Long
+
+  @Modifying
+  @Query("""
+    UPDATE /* class: MerchantDao, query: insert-merchant */ merchants
+    set name=:name,
+      creation_date=:creation_date,
+      email=:email,
+      phone=:phone,
+      address=:address
+      WHERE id=:id
+    """
+  )
+  fun update (
+    @Param("id") id: Long,
+    @Param("name") name: String,
+    @Param("creation_date") creationDate: LocalDate,
+    @Param("email") email: String,
+    @Param("phone") phone: String,
+    @Param("address") address: String
+  ): Long
+
+  @Query("""
+    SELECT merchants.id      AS id,
+     merchants.NAME          AS NAME,
+     merchants.creation_date AS creation_date,
+     merchants.email         AS email,
+     merchants.phone         AS phone,
+     merchants.address       AS address,
+     merchants.balance       AS balance,
+     merchants.credit        AS credit,
+     PLAN.id                 AS plan_id,
+     PLAN.NAME               AS plan_name,
+     PLAN.fee                AS plan_fee,
+     sale.id                 AS sales_id,
+     sale.product            AS sales_product,
+     sale.amount             AS sales_amount,
+     sale.creation_date      AS sales_creation_date
+    FROM merchants
+     LEFT JOIN plans AS PLAN ON PLAN.id = merchants.plan_id
+     LEFT JOIN sales AS sale ON sale.merchant_id = merchants.id
+    WHERE  merchants.id = :id
+    """)
+  fun getById(@Param("id") id: Long): Merchant
+
+  @Query("""
+    update merchants set plan_id = :idPlan where id = :id
+  """)
+  fun updatePlan(@Param("id") id: Long, @Param("idPlan") idPlan: Long): Long
+
+  @Query("""
+    insert into sales(id, creation_date, product, amount, merchant_id)
+    values(:idSale, :creationDate, :product, amount, :idMerchant)
+  """)
+  fun saveSale(
+    @Param("id") idSale: Long,
+    @Param("creationDate") creationDate: LocalDate,
+    @Param("product") product: String,
+    @Param("amount") amount: Double,
+    @Param("idMerchant") merchant: Long
+  ): Long
+}
